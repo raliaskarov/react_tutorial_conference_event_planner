7aab06d29e1c9b35096b5719d2d204506e9b0869
Roman Aliaskarov <r.aliaskarov@gmail.com>
2025-01-30 10:09:46 +0100

    Finished practice final task

--- src/TotalCost.jsx
+++ src/TotalCost.jsx
@@ -1,8 +1,12 @@
+// TotalCost.jsx
 import React, { useState, useEffect } from 'react';
 import "./TotalCost.css";
 
 const TotalCost = ({ totalCosts, ItemsDisplay }) => {
-  
+
+  console.log("Received Total Costs:", totalCosts); // Debugging line
+
+  const total_amount = totalCosts.venue + totalCosts.av + totalCosts.meals;
 
   return (
     <div className="pricing-app">
@@ -12,12 +16,12 @@ const TotalCost = ({ totalCosts, ItemsDisplay }) => {
         </div>
         <div>
           <h2 id="pre_fee_cost_display" className="price">
-           
+            ${total_amount}
           </h2>
-         
-            <div>
-             
-            </div>
+           <div className="render_items">
+           <ItemsDisplay />
+
+           </div>
         </div>
       </div>
     </div>
